54134f68fa9eb46b02df3558d537338d1d1199e5
Marthe1304 <marthe.mannott@posteo.de>
2024-09-11 19:06:58 +0200

    Delete final_project_v1 directory

--- final_project_v1/09.08./test.py
+++
@@ -1,9 +0,0 @@
-appending_list = [3, 4, 6, 7, 8, 7, 6, 4, 3, 4, 7, 8, 8, 8, 8, 8, 7, 4, 6, 7, 7, 7, 8, 7, 7, 7, 6, 7, 7, 8, 7, 8, 7, 8, 7, 7, 8, 7, 7, 7, 8, 7, 7, 7, 8, 7, 8, 8, 8, 8, 8, 8, 8, 7, 6, 8, 8, 8, 8, 8, 8, 8, 6, 4, 7, 8, 8, 8, 8, 8, 7, 4, 3, 4, 6, 7, 8, 7, 6, 4, 3]
-length_x_axis = 13
-length_y_axis = 13
-
-
-for y in range(length_y_axis - 2):
-    for x in range(length_x_axis - 2):
-        print (x, y)
-        #print (appending_list[y * length_y_axis + x])
